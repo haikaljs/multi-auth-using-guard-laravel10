7ce5373a577ae886dd659dc5293bbfd716c21164
haikaljs <haikaljavascript@gmail.com>
2023-08-10 12:32:00 +0800

    Write logic for navigation

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,13 @@
-<a href="{{ route('home') }}">Home</a> -
-    <a href="{{ route('dashboard') }}">Dashboard</a> -
-    <a href="{{ route('login') }}">Login</a> -
-    <a href="{{ route('register') }}">Registration</a> -
-    <a href="{{ route('logout') }}">Logout</a>
+<a href="{{ route('home') }}">Home</a>
+
+    @if (Auth::guard('web')->user())
+        <a href="{{ route('dashboard') }}">Dashboard</a> -
+        <a href="{{ route('logout') }}">Logout</a>
+    @endif
+
+    @if (!Auth::guard('web')->user())
+        <a href="{{ route('login') }}">Login</a> -
+        <a href="{{ route('register') }}">Registration</a> 
+    @endif
+ 
+   
